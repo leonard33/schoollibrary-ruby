0e8a85ddb3d5c3aab2235eeb141a30c8a69d1708
leonard33 <leonardkombo14@gmail.com>
2022-07-23 23:03:08 +0300

    add executable app

--- person.rb
+++ person.rb
@@ -1,36 +1,67 @@
-require_relative 'namable'
-require_relative 'decorator'
-require_relative 'rental'
+# require_relative './nameable.rb'
 
-class Person < Namable
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
-    @id = rand(1..100)
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
+# Person inherit from Nameable
+class Person
+  # getters and setters
+  attr_reader :id
+  attr_accessor :name, :age
+
+  # contructor
+  def initialize(age, name, parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_accessor :name, :age, :parent_permission
-
-  def can_use_service?
-    of_age? || @parent_permission
-  end
-
-  def add_rental(person, date)
-    Rental.new(date, self, person)
-  end
-
-  private
 
   def of_age?
     @age >= 18
   end
+
+  #  can use service method
+  def can_use_service?
+    @parent_permission || is_of_age?
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
+  private :of_age?
 end
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmedperson = TrimmerDecorator.new(capitalized_person)
-capitalized_trimmedperson.correct_name
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super
+    @component = nameable
+  end
+
+  def correct_name
+    @component.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @component.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @component.correct_name.chars.slice(0, 9).join
+  end
+end
--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,11 @@
-require './person'
+require_relative './person'
 
 class Teacher < Person
-  def initialize(age, specialisation, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
-    @specialisation = specialisation
+  def initialize(age, name, specialization)
+    super(age, name)
+    @specialization = specialization
+    @age = age
+    @name = name
   end
 
   def can_use_service?
